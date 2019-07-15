b24763a8491f6020404c23a809309196ed12a372
19mddil <mddilshadul98@gmail.com>
2019-07-16 01:11:48 +0600

    incredible for loop of python uses list

--- python/chapter1/odd.py
+++ python/chapter1/odd.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 odds = [1,3,5,7,9,11,13,15,17,19,
-		21,23,25,27,39,31,33,35,37,39,
+		21,23,25,27,29,31,33,35,37,39,
 		41,43,45,49,51,53,55,57,59]
+for i in odds:
+	print(i)
 right_this_minute = datetime.today().minute
 if right_this_minute in odds:
 	print("This minute seems a little odd.")
