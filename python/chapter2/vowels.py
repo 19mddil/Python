c42762389f54cb074eab5bdbc80a3a500dc6ec7a
19mddil <mddilshadul98@gmail.com>
2019-07-18 16:49:02 +0600

    testing 1

--- python/chapter2/vowels.py
+++ python/chapter2/vowels.py
@@ -1,5 +1,9 @@
 word = "milliways"
 vowels = "aeiou"
+#testing 
+found = []
+len(found)
+print(len(found))#in command >>> we don't have to use print
 for letter in word:
 	if letter in vowels:
 		print(letter)
