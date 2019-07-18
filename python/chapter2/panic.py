1a4a9c7d088b225a34e61552910a4d70145e4eb8
19mddil <mddilshadul98@gmail.com>
2019-07-18 23:32:32 +0600

    see the difference

--- python/chapter2/panic.py
+++ python/chapter2/panic.py
@@ -2,9 +2,6 @@ phrase = "Don't panic!"
 plist = list(phrase)#turing string into list
 print(phrase)
 print(plist)
-for i in range(4):
-	plist.pop()
-#Don't pa
 new_phrase = "".join(plist[1:3])
 print(new_phrase)
 new_phrase = new_phrase+"".join([plist[5],plist[4]])
