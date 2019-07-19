dc2d336626570630a4684238c806d8056e4cb554
19mddil <mddilshadul98@gmail.com>
2019-07-20 02:49:36 +0600

    keyerror with is noexistent key or key not initialised

--- python/chapter3/vowelsdictionary.py
+++ python/chapter3/vowelsdictionary.py
@@ -1,6 +1,6 @@
 word = input("Give a word to search for vowels:")
 vowels = "aeiou"#we could have written it like this vowels=['a','e','i','o','u']
-found = {'e':0,'i':0,'u':0,'o':0,'a':0}
+found = {}
 for letter in word:
 	if letter in vowels:
 		found[letter] += 1
