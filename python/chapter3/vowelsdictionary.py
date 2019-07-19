1fb10357e1b76547fb4fe011967b88b7267cb34b
19mddil <mddilshadul98@gmail.com>
2019-07-20 01:21:21 +0600

    dictionary version of vowel.py list has been coded

--- python/chapter3/vowelsdictionary.py
+++ python/chapter3/vowelsdictionary.py
@@ -1,6 +1,9 @@
+word = input("Give a word to search for vowels:")
+vowels = "aeiou"#we could have written it like this vowels=['a','e','i','o','u']
 found = {'e':0,'i':0,'u':0,'o':0,'a':0}
-k = 98
-v = 45
-print(k,v)
+for letter in word:
+	if letter in vowels:
+		found[letter] += 1
+		print(found[letter])
 for k,v in sorted(found.items()):
 	print(k,':',v)
