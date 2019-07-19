6668e148ca85e6283d0cd545ac0ed61a9b4e899c
19mddil <mddilshadul98@gmail.com>
2019-07-20 03:07:19 +0600

    fixing the key error with tradditonal if else technique

--- python/chapter3/vowelsdictionary.py
+++ python/chapter3/vowelsdictionary.py
@@ -3,7 +3,9 @@ vowels = "aeiou"#we could have written it like this vowels=['a','e','i','o','u']
 found = {}
 for letter in word:
 	if letter in vowels:
-		found[letter] += 1
-		#print(found[letter])
+		if letter not in found:
+			found[letter] = 1
+		else:
+			found[letter] += 1
 for k,v in sorted(found.items()):
 	print(k,':',v)
