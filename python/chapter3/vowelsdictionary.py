1332c32061436da2f9066085c0dc9b70b9a687fe
19mddil <mddilshadul98@gmail.com>
2019-07-20 03:09:45 +0600

    using setdefault instead of traditional if else for keyinint error

--- python/chapter3/vowelsdictionary.py
+++ python/chapter3/vowelsdictionary.py
@@ -3,9 +3,7 @@ vowels = "aeiou"#we could have written it like this vowels=['a','e','i','o','u']
 found = {}
 for letter in word:
 	if letter in vowels:
-		if letter not in found:
-			found[letter] = 1
-		else:
-			found[letter] += 1
+		found.setdefault(letter,0)
+		found[letter] += 1
 for k,v in sorted(found.items()):
 	print(k,':',v)
