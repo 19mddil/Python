4ec5cc236541150bc8b1a5737d00639009154f7c
19mddil <mddilshadul98@gmail.com>
2019-07-20 03:12:56 +0600

    added some comments

--- python/chapter3/vowelsdictionary.py
+++ python/chapter3/vowelsdictionary.py
@@ -3,7 +3,7 @@ vowels = "aeiou"#we could have written it like this vowels=['a','e','i','o','u']
 found = {}
 for letter in word:
 	if letter in vowels:
-		found.setdefault(letter,0)
+		found.setdefault(letter,0)#it only executes if not init a key
 		found[letter] += 1
 for k,v in sorted(found.items()):
 	print(k,':',v)
