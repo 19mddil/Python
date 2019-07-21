ca8d7947efe4b3c679b32c312539a79e3b6842b8
19mddil <mddilshadul98@gmail.com>
2019-07-21 23:52:24 +0600

    little question on how I can get rid of those spaces

---
+++ python/chapter3/dic_in_dic.py
@@ -0,0 +1,10 @@
+people = {}
+people['Ford'] = { 'Name': 'Ford Prefect','Gender': 'Male','Occupation': 'Researcher','Home Planet': 'Betelgeuse Seven' }
+people['Arthur'] = { 'Name': 'Arthur Dent','Gender': 'Male','Occupation': 'Sandwich-Maker','Home Planet': 'Earth' }
+people['Trillian'] = { 'Name': 'Tricia McMillan','Gender': 'Female','Occupation': 'Mathematician','Home Planet': 'Earth' }
+people['Robot'] = { 'Name': 'Marvin','Gender': 'Unknown','Occupation': 'Paranoid Android','Home Planet': 'Unknown' }
+for dic in people:
+	print(dic,":")
+	for dic2 in people[dic]:
+		print(dic2,':','"',people[dic][dic2],'"',end = " ")
+	print()
