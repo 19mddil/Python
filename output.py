ad63369775c6ac98c678faf3d27f740305d0f8fa
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2020-06-14 11:02:39 +0600

    output formatting

---
+++ output.py
@@ -0,0 +1,11 @@
+num = input("Enter a number: ")
+num = int(num)
+if not(num%2):
+    
+    #print(num,end=' ')
+    #print("is Even")
+    print(f"{num} is Even")
+else:
+    #print(num,end=' ')
+    #print("is Odd")
+    print(str(num)+" is Odd")
