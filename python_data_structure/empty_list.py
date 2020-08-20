486a261a81aad7224717ea8a894904003b8a4a68
19mddil <mddilshadul98@gmail.com>
2020-08-20 06:06:09 +0600

    lists are mutable and strings are immutable

--- python_data_structure/empty_list.py
+++ python_data_structure/empty_list.py
@@ -1,1 +1,12 @@
+#it is a empty list
 print([])
+
+fruit = 'Banana'
+try:
+    fruit[0] = 'b'
+except:
+    print('Strings are not mutable aka immutable')
+#But lists are mutable
+lotto = [2,3,4,5,'j']
+lotto[-1] = 10
+print(lotto) #as lists are mutable.
