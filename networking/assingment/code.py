9b0281626e6d043b51f37a5a52e33f84ebb14967
19mddil <mddilshadul98@gmail.com>
2020-08-22 07:27:33 +0600

    added some modification on web crawling code

--- networking/assingment/code.py
+++ networking/assingment/code.py
@@ -1,16 +1,31 @@
-from urllib.request import urlopen
+import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
+import re
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+def twk( url ,pos):
+    # Ignore SSL certificate errors
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
-html = urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, "html.parser")
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    # Retrieve all of the anchor tags
+    tags = soup('a')
+    #lnk = tags[pos-1].decode()
+    lnk = tags[pos-1].get('href',None)
+    return lnk
 
-# Retrieve all of the span tags
-tags = soup('span')
-print( sum( [ int(tag.contents[0]) for tag in tags ] ) )
+starting_url = input('Enter URL: ')
+count = int(input('Enter count: '))
+position = int(input('Enter position: '))
+temp = None
+for i in range(count):
+    if temp is None:
+        temp = starting_url
+        print('Retrieving:',temp)
+    #temp = re.findall('href="(.+)"',twk(temp,position))[0]
+    temp = twk(temp,position)
+    print('Retrieving:',temp)
