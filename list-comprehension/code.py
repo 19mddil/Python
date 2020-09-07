f5a11f9e7f78e2c0648f28b848558ecf11b32757
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2020-09-07 15:30:13 +0600

    Update code.py

--- list-comprehension/code.py
+++ list-comprehension/code.py
@@ -6,4 +6,4 @@ for a in lowercase_alph:
       for d in numbers:
         print(a+b+c+d) #generating all possible 4 digit passwords starting with two characters
 #list comprehension
-for password in [a+b+c+d for a in lowercase_alph for b in lowercase_alph for c in numbers for d in numbers]: print(numbers)
+for password in [a+b+c+d for a in lowercase_alph for b in lowercase_alph for c in numbers for d in numbers]: print(passwords)
