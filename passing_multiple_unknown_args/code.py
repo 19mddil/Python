f8ee2067a8ea3cf086def87068986b49cf6e2c3e
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2020-09-07 15:23:14 +0600

    Update code.py

--- passing_multiple_unknown_args/code.py
+++ passing_multiple_unknown_args/code.py
@@ -7,7 +7,13 @@ def calculateTotalSum(*arguments): #arguments passed behaves like a tuple
 	for number in arguments: 
 		totalSum += number 
 	print(totalSum) 
+def dasterisk(arg,**param):
+	print(arg)
+	print(type(param))
+	for (k,v) in param.items():
+		print("key :",k," Value:",v)
 
 # function call 
-calculateTotalSum(5, 4, 3, 2, 1) 
+calculateTotalSum(5, 4, 3, 2, 1)
+dasterisk('Dilshad',a=1,b=2,c=3)
 
