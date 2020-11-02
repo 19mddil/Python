915fa5fb761a4b8d6256b58ee6502dcd48891e8e
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2020-11-03 01:46:57 +0600

    Update revisited.py

--- regular_expression/revisited.py
+++ regular_expression/revisited.py
@@ -11,3 +11,27 @@ if re.search('^Amy',text): #checks if starts with Amy
     print('ok')
 else:
     print('not ok')
+    
+# About patterns
+## Character classes
+grades = 'ACAAAABCBCBAA'
+re.findall('B',grades) # ['B', 'B', 'B']
+re.findall('[AB]',grades) # set operator in use to find the occuraces of A and B's perticularly ['A', 'A', 'A', 'A', 'A', 'B', 'B', 'B', 'A', 'A']
+
+re.findall('AB',grades) # to find all occurannes of AB exactly ['AB']
+re.findall('AC',grades) # to find all occurances of AC exactly ['AC']
+#similarly
+re.findall('AB|AC',grades) # ['AC', 'AB']
+#more smart similarly
+re.findall('[A][B-C]',grades) #means can be AB or AC ['AC', 'AB']
+re.findall('^[^C]',grades) #means should start with A or B
+#like
+print(re.findall('^A|^B',grades))
+
+re.findall('[^B]B',grades) # AB,CB
+#like
+re.findall('AB|CB',grades)
+#like
+re.findall('AB',grades) + re.findall('CB',grades)
+
+
